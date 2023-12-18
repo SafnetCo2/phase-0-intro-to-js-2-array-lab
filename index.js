234c409bb547d8b250bad0ee74fae376c91bf4f8
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-18 16:15:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    let catsArr = [...cats, name]
+    return catsArr;
+}
+function prependCat(name) {
+    let catsCopy = [name, ...cats]
+    return catsCopy;
+}
+function removeLastCat() {
+    //let catsLast = cats.slice(0, cats.length - 1)
+    //return catsLast;
+    return cats.slice(0, -1)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
